9eba01563f4c4d7a56af033bea5ad6f3ae869050
catalin1989 <catalin_chira05@yahoo.com>
2023-04-20 10:42:00 +0300

    am creat un buzunar

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Buzunar buzunar= new Buzunar();
+
+        buzunar.push(new String("unu"));
+        buzunar.push(new String("doi"));
+        buzunar.push(new String("trei"));
+        buzunar.push(new String("patru"));
+        buzunar.listIterator();
+        try {
+            for (int i=0; buzunar.listIterator().hasNext();i++){
+                System.out.println(buzunar.get(i));
+            }
+
+
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Am ajuns la capatul Stivei");
+
+        }
+
+        System.out.println(buzunar.size());
+
+        for (int i=buzunar.size();buzunar.listIterator().hasNext();i--){
+            System.out.println(buzunar.pop());
+
+        }
+
+
     }
 }
